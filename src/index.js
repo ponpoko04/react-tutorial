2656a0680cef0f529f07ac2493db1edc18724ad3
Takyoshi Uchida <taum0715@gmail.com>
2018-11-14 00:05:38 +0900

    Function Components

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => { this.props.onClick(); }}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        /* function componentsではthisの心配をする必要がないので、arrow functionではなくより短いプロパティ渡しで書いている */
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
