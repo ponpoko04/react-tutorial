d2716359d140beed3d6bd735f058f854343a7e37
Takyoshi Uchida <taum0715@gmail.com>
2018-11-14 23:56:15 +0900

    Implementing Time Travel

--- src/index.js
+++ src/index.js
@@ -49,12 +49,13 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -65,13 +66,21 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
         });
     }
     
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
@@ -79,12 +88,7 @@ class Game extends React.Component {
                 'Go to move #' + move :
                 'Go to game start';
             return (
-                /* 以下の例外が発生する */
-                // index.js:1452 Warning: Each child in an array or iterator should have a unique "key" prop.
-                // Check the render method of `Game`. See https://fb.me/react-warning-keys for more information.
-                // in li (at src/index.js:82)
-                // in Game (at src/index.js:113)
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
